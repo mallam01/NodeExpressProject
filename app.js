7dc50132eda8f58d44aa2c79e0f7be9e73af7065
Mahmoud R.Allam <m.roushdy.allam@gmail.com>
2024-11-28 01:02:00 -0600

    typo issue fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port = procrdd.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 // npm packages require to MongoDB connection
 const mongoose = require('mongoose');
